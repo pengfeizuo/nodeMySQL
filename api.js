451e44774a94cdde169e451a1d004bbff4c33fa3
@mrzuo <pengfei.zuo@freebrio.com>
2019-11-04 16:51:28 +0800

    扩展

--- api.js
+++ api.js
@@ -2,9 +2,17 @@
 const {pool,router,Result,app}=require('./config/connect');
 
 function api (){
+	app.post('/login',(req,res)=>{
+		var selectSQL = "select account,password from user where account = '"+req.query.account+"' and password = '"+req.query.password+"'";
+		pool.getConnection((err,conn)=>{
+			conn.query(selectSQL,(e,r)=>res.json(
+				new Result({data:r})
+				));
+			conn.release();
+		})
+	});
 	app.post('/adduser',(req,res)=>{
 		var  addSql = "INSERT INTO `node_test`.`user` (`account`, `password`) VALUES ('"+req.body.account+"', '"+req.body.password+"')";
-		var data = JSON.parse(JSON.stringify(req.body)) 
 		pool.getConnection((err,conn)=>{
 			conn.query(addSql,(e,r)=>res.json(new Result({data:r})));
 			conn.release();
